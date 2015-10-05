89bf92b08cff6644d24a45d7a0f6b69d95f8de34
victor <bocharsky.bw@gmail.com>
2015-10-05 17:17:23 +0300

    Move all abstract methods to the beginning of the class

--- src/AbstractArray.php
+++ src/AbstractArray.php
@@ -60,237 +60,6 @@ abstract class AbstractArray implements
     }
 
     /**
-     * Create a new instance
-     *
-     * @param array $elements
-     *
-     * @return static Returns created instance
-     */
-    public static function create(array $elements = [])
-    {
-        return new static($elements);
-    }
-
-    /**
-     * Create a new instance from the implemented ArrayAccess instance
-     *
-     * @param ArrayAccess $elements Object that implements the ArrayAccess
-     *
-     * @return static Returns created instance
-     */
-    public static function createFromObject(ArrayAccess $elements)
-    {
-        $array = new static();
-
-        foreach ($elements as $key => $value) {
-            $array[$key] = $value;
-        }
-
-        return $array;
-    }
-
-    /**
-     * Decode a JSON string to new instance
-     *
-     * @param string $json The json string being decoded
-     * @param int $options Bitmask of JSON decode options
-     * @param int $depth Specified recursion depth
-     *
-     * @return static Returns created instance
-     */
-    public static function createFromJson($json, $options = 0, $depth = 512)
-    {
-        return new static(json_decode($json, true, $depth, $options));
-    }
-
-    /**
-     * Explode a string to new instance by specified separator
-     *
-     * @param string $string Converted string
-     * @param string $separator Element's separator
-     *
-     * @return static Returns created instance
-     */
-    public static function createFromString($string, $separator)
-    {
-        return new static(explode($separator, $string));
-    }
-
-
-    /**
-     * Create a new instance containing a range of elements
-     *
-     * @param mixed $low First value of the sequence
-     * @param mixed $high The sequence is ended upon reaching the end value
-     * @param int $step Used as the increment between elements in the sequence
-     *
-     * @return static Returns created instance
-     */
-    public static function createWithRange($low, $high, $step = 1)
-    {
-        return new static(range($low, $high, $step));
-    }
-
-    /**
-     * Clone current instance to new instance
-     *
-     * @return $this Returns cloned instance
-     */
-    public function createClone()
-    {
-        return clone $this;
-    }
-
-    /**
-     * Check whether array is empty or no
-     *
-     * @return bool Returns true if empty, false otherwise
-     */
-    public function isEmpty()
-    {
-        return !$this->elements;
-    }
-
-    /**
-     * Check if the given value exists in array
-     *
-     * @param mixed $element Value to search
-     *
-     * @return bool Returns true if the given value exists in array, false otherwise
-     */
-    public function contains($element)
-    {
-        return in_array($element, $this->elements, true);
-    }
-
-    /**
-     * Check if the given key/index exists in array
-     *
-     * @param mixed $key Key/index to search
-     *
-     * @return bool Returns true if the given key/index exists in array, false otherwise
-     */
-    public function containsKey($key)
-    {
-        return array_key_exists($key, $this->elements);
-    }
-
-    /**
-     * Search in array for a given element
-     *
-     * @param mixed $element Value to search key/index
-     *
-     * @return mixed The corresponding key/index
-     */
-    public function indexOf($element)
-    {
-        return array_search($element, $this->elements, true);
-    }
-
-    /**
-     * Pick a random key/index out key of array
-     *
-     * @return mixed Random key/index of array
-     *
-     * @throws \LogicException
-     */
-    public function getRandomKey()
-    {
-        if (1 >= $this->count()) {
-            throw new \LogicException(sprintf(
-                'The number of elements in the array "%d" should be greater than "1".', $this->count()
-            ));
-        }
-
-        return array_rand($this->elements, 1);
-    }
-
-    /**
-     * Pick a random value out of array
-     *
-     * @return mixed Random value of array
-     *
-     * @throws \LogicException
-     */
-    public function getRandom()
-    {
-        return $this->offsetGet($this->getRandomKey());
-    }
-
-    /**
-     * Pick a given number of random keys/indexes out of array
-     *
-     * @param int $number The number of keys/indexes (should be > 1 and < $this->count())
-     *
-     * @return array Random keys of array
-     *
-     * @throws \RangeException
-     * @throws \LogicException
-     */
-    public function getRandomKeys($number)
-    {
-        $number = (int) $number;
-        $count = $this->count();
-        if ($number < 1 || $number > $count) {
-            throw new \RangeException(sprintf(''
-                . 'The given number "%d" should be greater than or equal to "1" '
-                . 'and less than or equal to "%d" (the number of elements in the array).',
-                $number,
-                $count
-            ));
-        }
-        if ($number === 1 || $number === $count) {
-            throw new \LogicException(sprintf(
-                'The given number "%d" should not be equal to "1" or "%d" (the number of elements in the array).',
-                $number,
-                $count
-            ));
-        }
-
-        return array_rand($this->elements, $number);
-    }
-
-    /**
-     * Pick a given number of random values out of array
-     *
-     * @param int $number The number of values (should be > 1 and < $this->count())
-     *
-     * @return array Random values of array
-     *
-     * @throws \RangeException
-     * @throws \LogicException
-     */
-    public function getRandomValues($number)
-    {
-        $values = [];
-        foreach ($this->getRandomKeys($number) as $key) {
-            $values[] = $this->offsetGet($key);
-        }
-
-        return $values;
-    }
-
-    /**
-     * Return all the keys of array
-     *
-     * @return array An array of all keys
-     */
-    public function getKeys()
-    {
-        return array_keys($this->elements);
-    }
-
-    /**
-     * Return all the values of array
-     *
-     * @return mixed An array of all values
-     */
-    public function getValues()
-    {
-        return array_values($this->elements);
-    }
-
-    /**
      * Reindex the array
      *
      * @return static The instance with re-indexed elements
@@ -488,6 +257,243 @@ abstract class AbstractArray implements
     abstract public function customSortKeys(Closure $func);
 
     /**
+     * Clear array
+     *
+     * @return static The instance with cleared array
+     */
+    abstract public function clear();
+
+    /**
+     * Create a new instance
+     *
+     * @param array $elements
+     *
+     * @return static Returns created instance
+     */
+    public static function create(array $elements = [])
+    {
+        return new static($elements);
+    }
+
+    /**
+     * Create a new instance from the implemented ArrayAccess instance
+     *
+     * @param ArrayAccess $elements Object that implements the ArrayAccess
+     *
+     * @return static Returns created instance
+     */
+    public static function createFromObject(ArrayAccess $elements)
+    {
+        $array = new static();
+
+        foreach ($elements as $key => $value) {
+            $array[$key] = $value;
+        }
+
+        return $array;
+    }
+
+    /**
+     * Decode a JSON string to new instance
+     *
+     * @param string $json The json string being decoded
+     * @param int $options Bitmask of JSON decode options
+     * @param int $depth Specified recursion depth
+     *
+     * @return static Returns created instance
+     */
+    public static function createFromJson($json, $options = 0, $depth = 512)
+    {
+        return new static(json_decode($json, true, $depth, $options));
+    }
+
+    /**
+     * Explode a string to new instance by specified separator
+     *
+     * @param string $string Converted string
+     * @param string $separator Element's separator
+     *
+     * @return static Returns created instance
+     */
+    public static function createFromString($string, $separator)
+    {
+        return new static(explode($separator, $string));
+    }
+
+    /**
+     * Create a new instance containing a range of elements
+     *
+     * @param mixed $low First value of the sequence
+     * @param mixed $high The sequence is ended upon reaching the end value
+     * @param int $step Used as the increment between elements in the sequence
+     *
+     * @return static Returns created instance
+     */
+    public static function createWithRange($low, $high, $step = 1)
+    {
+        return new static(range($low, $high, $step));
+    }
+
+    /**
+     * Clone current instance to new instance
+     *
+     * @return $this Returns cloned instance
+     */
+    public function createClone()
+    {
+        return clone $this;
+    }
+
+    /**
+     * Check whether array is empty or no
+     *
+     * @return bool Returns true if empty, false otherwise
+     */
+    public function isEmpty()
+    {
+        return !$this->elements;
+    }
+
+    /**
+     * Check if the given value exists in array
+     *
+     * @param mixed $element Value to search
+     *
+     * @return bool Returns true if the given value exists in array, false otherwise
+     */
+    public function contains($element)
+    {
+        return in_array($element, $this->elements, true);
+    }
+
+    /**
+     * Check if the given key/index exists in array
+     *
+     * @param mixed $key Key/index to search
+     *
+     * @return bool Returns true if the given key/index exists in array, false otherwise
+     */
+    public function containsKey($key)
+    {
+        return array_key_exists($key, $this->elements);
+    }
+
+    /**
+     * Search in array for a given element
+     *
+     * @param mixed $element Value to search key/index
+     *
+     * @return mixed The corresponding key/index
+     */
+    public function indexOf($element)
+    {
+        return array_search($element, $this->elements, true);
+    }
+
+    /**
+     * Pick a random key/index out key of array
+     *
+     * @return mixed Random key/index of array
+     *
+     * @throws \LogicException
+     */
+    public function getRandomKey()
+    {
+        if (1 >= $this->count()) {
+            throw new \LogicException(sprintf(
+                'The number of elements in the array "%d" should be greater than "1".', $this->count()
+            ));
+        }
+
+        return array_rand($this->elements, 1);
+    }
+
+    /**
+     * Pick a random value out of array
+     *
+     * @return mixed Random value of array
+     *
+     * @throws \LogicException
+     */
+    public function getRandom()
+    {
+        return $this->offsetGet($this->getRandomKey());
+    }
+
+    /**
+     * Pick a given number of random keys/indexes out of array
+     *
+     * @param int $number The number of keys/indexes (should be > 1 and < $this->count())
+     *
+     * @return array Random keys of array
+     *
+     * @throws \RangeException
+     * @throws \LogicException
+     */
+    public function getRandomKeys($number)
+    {
+        $number = (int) $number;
+        $count = $this->count();
+        if ($number < 1 || $number > $count) {
+            throw new \RangeException(sprintf(''
+                . 'The given number "%d" should be greater than or equal to "1" '
+                . 'and less than or equal to "%d" (the number of elements in the array).',
+                $number,
+                $count
+            ));
+        }
+        if ($number === 1 || $number === $count) {
+            throw new \LogicException(sprintf(
+                'The given number "%d" should not be equal to "1" or "%d" (the number of elements in the array).',
+                $number,
+                $count
+            ));
+        }
+
+        return array_rand($this->elements, $number);
+    }
+
+    /**
+     * Pick a given number of random values out of array
+     *
+     * @param int $number The number of values (should be > 1 and < $this->count())
+     *
+     * @return array Random values of array
+     *
+     * @throws \RangeException
+     * @throws \LogicException
+     */
+    public function getRandomValues($number)
+    {
+        $values = [];
+        foreach ($this->getRandomKeys($number) as $key) {
+            $values[] = $this->offsetGet($key);
+        }
+
+        return $values;
+    }
+
+    /**
+     * Return all the keys of array
+     *
+     * @return array An array of all keys
+     */
+    public function getKeys()
+    {
+        return array_keys($this->elements);
+    }
+
+    /**
+     * Return all the values of array
+     *
+     * @return mixed An array of all values
+     */
+    public function getValues()
+    {
+        return array_values($this->elements);
+    }
+
+    /**
      * Iteratively reduce array to a single value using a function
      *
      * @param Closure $func
@@ -499,13 +505,6 @@ abstract class AbstractArray implements
     {
         return array_reduce($this->elements, $func, $initial);
     }
-
-    /**
-     * Clear array
-     *
-     * @return static The instance with cleared array
-     */
-    abstract public function clear();
 
     /**
      * Counts all elements in array
